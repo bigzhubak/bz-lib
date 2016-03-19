43d65f2daf01231fd7c36ba17ab4ec436456250c
bigzhu <vermiliondun@gmail.com>
2016-03-19 14:14:13 +0800

    可以替换hash了

--- p_src/update_js_version.py
+++ p_src/update_js_version.py
@@ -12,13 +12,14 @@ def getScripts(html):
     return scripts
 
 
-def getScripthash(script):
+def getScriptHash(script):
     src = script.get('src')
     script_file = re.sub('\?v=.*', '', src)
     f = open('../' + script_file, 'r')
     script_content = f.read()
     hash = hashlib.sha224(script_content).hexdigest()
     f.close()
+    print 'new hash=%s' % hash
     return hash
 
 if __name__ == '__main__':
@@ -29,10 +30,13 @@ if __name__ == '__main__':
     #    print 'run as:'
     #    print 'python update_js_version.py index.html'
 
-    f = open('../index.html', 'r')
+    f = open('../index.html', 'rw')
     html = f.read()
     f.close()
     scripts = getScripts(html)
+    f = open('../index.html', 'w')
     for script in scripts:
-        hash = getScripthash(script)
-        print hash
+        hash = getScriptHash(script)
+        html = re.sub('\?v=.*"', '?v=%s"'%hash, html)
+        f.write(html)
+    f.close()
