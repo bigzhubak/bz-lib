42e11ec6ced1d1d290d9f5fba689dcff95e9c816
bigzhu <vermiliondun@gmail.com>
2016-05-16 16:22:16 +0800

    fix sytax bug

--- functions/random.js
+++ functions/random.js
@@ -1,4 +1,4 @@
-var colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black' ]
+var colors = [ 'red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black' ]
 import _ from 'underscore'
 export function color () {
   let index = _.random(0, colors.length)
