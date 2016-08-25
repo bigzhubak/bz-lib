387d9e97a694418f252a81daa6a18f8226c74797
bigzhu <vermiliondun@gmail.com>
2016-08-25 19:31:28 +0800

    暴露参数

--- public_store.js
+++ public_store.js
@@ -2,43 +2,77 @@
 import Vue from 'vue'
 var api_login = Vue.resource('/api_login{/parm}')
 var api_signup = Vue.resource('/api_signup{/parm}')
+var api_user_info = Vue.resource('/api_user_info{/parm}')
 // state
 export const state = {
+  user_info: {
+    user_name: '',
+    picture: ''
+  }
 }
 // mutations
 export const mutations = {
+  SET_USER_INFO (state, user_info) {
+    state.user_info = user_info
+  }
 }
 // actions
-export const login = (store, parm, call_back = null, error_call_back = null) => {
+export const login = (store, user_name, password, done = null, error = null) => {
+  let parm = {}
+  parm.user_name = user_name
+  parm.password = password
+
   api_login.save(parm).then(
     (response) => {
       if (response.data.error !== '0') {
-        if (error_call_back) error_call_back(response)
+        if (error) error(response)
         throw new Error(response.data.error)
       }
-      if (call_back) {
-        call_back()
+      if (done) {
+        done(response)
       }
     },
     (response) => {
-      if (error_call_back) error_call_back(response)
+      if (error) error(response)
     }
   )
 }
 
-export const signup = (store, parm, call_back = null, error_call_back = null) => {
+export const signup = (store, user_name, password, email, done = null, error = null) => {
+  let parm = {}
+  parm.user_name = user_name
+  parm.password = password
+  parm.email = email
   api_signup.save(parm).then(
     (response) => {
       if (response.data.error !== '0') {
-        if (error_call_back) error_call_back(response)
+        if (error) error(response)
         throw new Error(response.data.error)
       }
-      if (call_back) {
-        call_back()
+      if (done) {
+        done(response)
       }
     },
     (response) => {
-      if (error_call_back) error_call_back(response)
+      if (error) error(response)
+    }
+  )
+}
+
+export const queryUserInfo = (store, done = null, error = null) => {
+  api_user_info.query().then(
+    (response) => {
+      if (response.data.error !== '0') {
+        if (error) error(response)
+        throw new Error(response.data.error)
+      }
+      store.dispatch('SET_USER_INFO', response.data.user_info)
+      if (done) {
+        done(response)
+      }
+    },
+    (response) => {
+      if (error) error(response)
     }
   )
 }
