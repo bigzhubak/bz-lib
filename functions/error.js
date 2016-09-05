ed717b9dff94b87dcad8353a6f952666831a2a21
bigzhu <vermiliondun@gmail.com>
2016-09-05 11:13:24 +0800

    fix error

--- functions/error.js
+++ functions/error.js
@@ -1,5 +1,5 @@
 import toastr from 'toastr'
-export function errorHandle(message, source, lineno, colno, error) {
+export function errorHandle (message, source, lineno, colno, error) {
   toastr.error(error.message)
   return false
 }
