356b1ead88c75f0d227d8bce03cdcd1df9325f7d
bigzhu <vermiliondun@gmail.com>
2016-10-07 23:14:37 +0800

    use initErrorHandle

--- functions/error.js
+++ functions/error.js
@@ -6,3 +6,15 @@ export function errorHandle (message, source, lineno, colno, error) {
 export function errorHandlePromise (error, promise) {
   toastr.error(error.reason.message)
 }
+
+export function initErrorHandle () {
+  window.addEventListener('unhandledrejection',
+    function (error, promise) {
+      toastr.error(error.message)
+    }
+  )
+  window.onerror = function (message, source, lineno, colno, error) {
+    toastr.error(error.message)
+    return false
+  }
+}
